91e73492915ac6f434e4a7518ce367d1148f3329
Francesco Stefanni <francesco.stefanni@gizeroenergie.it>
2024-03-01 08:48:50 +0100

    files moved

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
-var Ber = require('./lib/ber/index')
+var Ber = requi../lib/ber/index.jsx')
 
 exports.Ber = Ber
 exports.BerReader = Ber.Reader
 exports.BerWriter = Ber.Writer
+../lib/ber/index.js
